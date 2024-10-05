6ac2b0e0d06cc7b878f4522512543fa0df23d2fc
murillowilmar1 <murillowilmar1@homtail.com>
2024-10-05 12:06:58 -0500

    publicar cambios

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template
+import pyodbc
 
-# Inicializa la aplicación Flask
 app = Flask(__name__)
 
-# Ruta principal que devuelve el archivo HTML
+# Configuración de la conexión a Azure SQL Database
+server = 'serverapp.database.windows.net'
+database = 'appdb'
+username = 'admins'
+password = 'Julietta20.,'
+driver = '{ODBC Driver 17 for SQL Server}'
+
+def get_db_connection():
+    connection = pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}')
+    return connection
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT TOP 5 * FROM Empleados")
+    empleados = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', empleados=empleados)
 
-# Ruta para el Health Check
-@app.route('/health')
-def health_check():
-    return 'Healthy', 200
-
-# Inicia el servidor si este archivo se ejecuta directamente
 if __name__ == '__main__':
     app.run(debug=True)
